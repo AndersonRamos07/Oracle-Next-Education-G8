ccff8618e1ea492e8e9f2974a5a99c5ea6a2b762
Anderson Ramos <andersonramos07@live.com>
2025-07-01 22:45:41 -0300

    feat: correcting the customConverter() and oneMoreConverter()

--- challenge-conversor-moeda/src/challenge/conversor/service/Functions.java
+++ challenge-conversor-moeda/src/challenge/conversor/service/Functions.java
@@ -5,7 +5,6 @@ import challenge.conversor.ui.Menu;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Functions {
@@ -22,6 +21,7 @@ public class Functions {
         int choosen = pTerminal.nextInt();
         pTerminal.nextLine();
 
+        int resposta = -1;
         String[] coins = {};
         ArrayList<String> response = null;
 
@@ -33,7 +33,7 @@ public class Functions {
             case 5 -> coins = new String[]{"HNL", "PAB"};
             case 6 -> coins = new String[]{"CRC", "DOP"};
             case 7 -> showList(pTerminal);
-            case 8 -> customConverter(pTerminal);
+            case 8 -> { resposta = customConverter(pTerminal); }
             case 9 -> System.out.println("Registros (logs)");
             case 0 -> sair();
         }
@@ -44,15 +44,21 @@ public class Functions {
             double valor = pTerminal.nextDouble();
             pTerminal.nextLine();
 
-            response = new ArrayList<String>(Arrays.asList(coins));
+            response = new ArrayList<>(Arrays.asList(coins));
             response.add(Double.toString(valor));
-        };
+        }
+        /*
         toOutput("CHOOSEN: ", "mesma linha");
         toOutput(choosen, "");
         toOutput("RESPONSE: ", "mesma linha");
         toOutput(response, "");
-
-        return response;
+        */
+        if(resposta != -1)
+        {
+            return resposta;
+        } else {
+            return response;
+        }
     }
 
     private static void showList(Scanner pTerminalList) { //7
@@ -77,42 +83,50 @@ public class Functions {
         else { sair(); }
     }
 
-    private static void customConverter(Scanner pTerminalCustom) { //8
-        String coinToConverter;
-        String coinConverted;
-        double valueToConverter;
+    private static int customConverter(Scanner pTerminalCustom) { //8
+        String coinToConverter = "";
+        String coinConverted = "";
+        double valueToConverter = 0.00;
 
-        toOutput("Qual moeda a ser convertida? (ex: BRL)", "");
+        try {
+            toOutput("\nQual moeda a ser convertida? (ex: BRL)", "");
             coinToConverter = pTerminalCustom.nextLine();
-        toOutput("Para qual moeda gostaria de converter? (ex: USD)", "");
+            toOutput("Para qual moeda gostaria de converter? (ex: USD)", "");
             coinConverted = pTerminalCustom.nextLine();
-        toOutput("Agora insira o valor a ser convertido: (ex: 70.00)", "");
+            toOutput("Agora insira o valor a ser convertido: (ex: 70.00)", "");
             valueToConverter = pTerminalCustom.nextDouble();
             pTerminalCustom.nextLine();
+        } catch(Exception e)
+        {
+            toOutput(e, "");
+        } finally{
+            Interacting api = new Interacting();
+            api.toRequire(coinToConverter.toUpperCase(),coinConverted.toUpperCase(),valueToConverter);
 
-        Interacting api = new Interacting();
-        api.toRequire(coinToConverter.toUpperCase(),coinConverted.toUpperCase(),valueToConverter);
-
-        oneMoreConverter(pTerminalCustom, "8");
+        }
+        return oneMoreConverter(pTerminalCustom, "8");
     }
 
     public static int oneMoreConverter(Scanner pTerminalOneMore, String option) {
         int response = 0;
 
-        int vOption = Integer.parseInt(option);
+        if(option.equals("8")){
+            toOutput("[custom]", "mesma linha");}
+        toOutput("\n[Gostaria de realizar mais uma conversao?]", "");
+            toOutput("[ -> 1) Para SIM/YES, -> 0) Para NAO/NO: ]", "");
+            response = pTerminalOneMore.nextInt();
+            pTerminalOneMore.nextLine();
 
-        toOutput("\n\n[Gostaria de realizar mais uma conversao?]", "");
-        toOutput("[ -> 1) Para SIM/YES, -> 2) Para NAO/NO: ]", "");
-        response = pTerminalOneMore.nextInt();
-        pTerminalOneMore.nextLine();
+        if( response == 0 && !option.equals("8")) { toOutput("OK...", ""); }
+        if( option.equals("8") && response != 0) { customConverter(pTerminalOneMore); }
 
-        if(response == 2 || response == 0) { vOption = 0; }
-
-        return vOption;
+        return response;
     }
+
     private static int toListLogs() { //9
         return -1;
     }
+
     public static void sair() { //0
         Menu menu = new Menu();
         toOutput(menu.epilog, "");
