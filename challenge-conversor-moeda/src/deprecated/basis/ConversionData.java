03d97d547959b0f0e0794fab04e7468fce2952ed
Anderson Ramos <andersonramos07@live.com>
2025-07-03 17:53:30 -0300

    delete: deleting deprecated directory and files

--- challenge-conversor-moeda/src/deprecated/basis/ConversionData.java
+++
@@ -1,8 +0,0 @@
-package deprecated.basis;
-
-public record ConversionData() {
-    static String currency;
-    static String currencyCode;
-    static double value;
-    static double exchangeValue;
-}
