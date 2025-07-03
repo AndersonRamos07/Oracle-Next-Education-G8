e9e838bc8f828c62f7da2e2502dcbdec7095b383
Anderson Ramos <andersonramos07@live.com>
2025-07-03 17:42:33 -0300

    feat: including handler file as a writer of logs

--- challenge-conversor-moeda/src/challenge/conversor/service/Conversor.java
+++ challenge-conversor-moeda/src/challenge/conversor/service/Conversor.java
@@ -1,27 +1,78 @@
 package challenge.conversor.service;
 
+import challenge.conversor.repository.HandlerFile;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import java.io.File;
+
 public class Conversor {
 
-    public double value;
+    public static double value;
+    static String currency;
+    static String currencyCode;
+    static double exchangeValue;
+    static LocalDateTime data;
+    static boolean yesOrNo;
+    static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy - HH:mm'h'");
+    /*
+    //public static File arquivo = new File("arquivoDeLogs.txt");
 
+//    public void conversion(String currency,
+//                           String currencyCode,
+//                           double exchangeValue,
+//                           double value,
+//                           LocalDateTime data)
+//    {
+//        this.currency = currency;
+//        this.currencyCode = currencyCode;
+//        this.exchangeValue = exchangeValue;
+//        this.value = value;
+//        this.data = data;
+//    }
+*/
     public void conversion(String currency,
                            String currencyCode,
                            double exchangeValue,
                            double value,
                            LocalDateTime data)    {
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        String formattedData = data.format(formatter);
+        this.currency = currency;
+        this.currencyCode = currencyCode;
+        this.exchangeValue = exchangeValue;
+        this.value = value;
+        this.data = data;
+
+        //DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mmh - dd/MM/yyyy");
+        //String formattedData = data.format(formatter);
 
         double resultConverted = exchangeValue * value;
-        System.out.printf("Taxa de %s em relação ao %s: %.2f",currency, currencyCode, resultConverted);
-        System.out.print("\nConversao realizada em: "+formattedData);
-        System.out.println("\n");
+
+        try {
+            System.out.printf("Taxa de %s em relação ao %s: %.2f", currency, currencyCode, resultConverted);
+            System.out.print("\nConversao realizada em: " + data.format(formatter));
+            System.out.println("\n");
+        } catch(Exception e){ e.printStackTrace();
+        } finally{
+            yesOrNo = true;
+
+            HandlerFile handle = new HandlerFile();
+                handle.appendLog(this.toString());
+        }
     }
 
+    @Override
+    public String toString() {
+        //DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy - HH:mm'h'");
+        return "{" +
+                "\ndata:" + data.format(formatter) +
+                ",\ncurrency:'" + currency + '\'' +
+                ",\nvalue:" + value +
+                ",\ncurrencyCode:'" + currencyCode + '\'' +
+                ",\nexchangeValue:" + exchangeValue +
+                "\n"+'}'+",\n";
+    }
     /*
     * String coinToConverter;
         String coinConverted;
@@ -37,5 +88,13 @@ public class Conversor {
 
         Interacting api = new Interacting();
         api.toRequire(coinToConverter,coinConverted,valueToConverter);
+        *
+        * //            String escrito = "\n{" +
+//                    "\ndata:" + data +
+//                    ",\ncurrency:'" + currency + '\'' +
+//                    ",\nvalue:" + value +
+//                    ",\ncurrencyCode:'" + currencyCode + '\'' +
+//                    ",\nexchangeValue:" + exchangeValue +
+//                    "\n"+'}'+",";
     * */
 }
