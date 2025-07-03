239e5061f7ba9b63107a45dc6cb8fadc4dceb371
Anderson Ramos <andersonramos07@live.com>
2025-07-03 17:47:59 -0300

    delete: deleting ConversorRecord

--- challenge-conversor-moeda/src/deprecated/model/ConversionRecord.java
+++ challenge-conversor-moeda/src/deprecated/model/ConversionRecord.java
@@ -1,9 +1,12 @@
 package challenge.conversor.model;
 
+import java.time.LocalDateTime;
+
 public record ConversionRecord(String currency,
                                String currencyCode,
                                double value,
-                               double exchangeValue) {
+                               double exchangeValue,
+                               LocalDateTime data) {
 /*    static String currency;
     static String currencyCode;
     static double value;
